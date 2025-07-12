11402fc5ca54fd1656a54332a7a83a1f934ffff6
Alex <sanoki.full@gmail.com>
2025-07-12 08:25:39 +0200

    try_rout_13

--- routeur.js
+++ routeur.js
@@ -1,4 +1,5 @@
 ;(function() {
+    console.log("Hi guy")
   const app = document.getElementById('app');
   const router = new Navigo('/', { hash: false });
 
@@ -16,8 +17,8 @@
   }
 
   router
-    .on('/', () => render('home.html'))
-    .on('/searched/:term+', ({ data }) => render('searched.html', data.term))
+    .on('/', () => render('index.html'))
+    .on('/searched/:term+', ({ data }) => render('indeed.html', data.term))
     .notFound(() => render('404.html', window.location.pathname))
     .resolve();
 
